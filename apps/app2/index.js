9bd945c08bbd450b2c93e8d5885a174ddcb66ba5
kunalp726 <kunalp726@gmail.com>
2021-04-11 11:28:43 -0700

    third commit

--- apps/app2/index.js
+++ apps/app2/index.js
@@ -2,6 +2,6 @@ function test(){
     console.log("Hello! This is my test app2222!!")
 }
 function test2(){
-    console.log("Hello! This is my test222 app2222!!")
+    console.log("Helllo! This is my test222 app2222!!")
 }
 module.exports = { test, test2 };
